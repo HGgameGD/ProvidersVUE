7474bb1d24ed6587e719fc64a02b31f09d2c02c9
Anibal <anibalhernandez787@gmail.com>
2026-03-19 12:47:46 -0600

    fix route /api/suppliers

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-      '/suppliers': {
+      '/api/suppliers': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
